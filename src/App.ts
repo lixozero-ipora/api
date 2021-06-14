e4346fadca4bd2efac494108b93adc0d44f3c0f4
João Pedro <pedrobarros2010@gmail.com>
2021-06-13 22:30:11 -0300

    feat: apply new routes and configuration

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,36 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import 'reflect-metadata'
+import 'express-async-errors'
+
+import UserRoutes from './routes/UserRoutes'
+import SessionRoutes from './routes/SessionRoutes'
+import ErrorMiddleware from './middleware/Error'
+import connection from './database/connection'
+
+connection()
 
 class App {
   server = express()
 
   constructor() {
+    dotenv.config()
     this.middlewares()
+    this.routes()
+    this.errorMiddleware()
   }
 
   middlewares() {
     this.server.use(express.json())
+  }
+
+  routes() {
+    this.server.use(UserRoutes)
+    this.server.use(SessionRoutes)
+  }
+
+  errorMiddleware() {
+    this.server.use(ErrorMiddleware)
   }
 }
 
