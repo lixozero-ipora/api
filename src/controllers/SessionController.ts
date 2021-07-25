059f37ce1a60ca15d9fe10131089363ec57977ab
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-25 15:58:07 -0300

    feat: create session based on env variable

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -1,23 +1,20 @@
 import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
 import JWT from 'jsonwebtoken'
-import { getCustomRepository } from 'typeorm'
-import UserRepository from '../database/repository/UserRepository'
 import AppError from '../utils/AppError'
 
 class SessionController {
   static async store(req: Request, res: Response) {
-    const { email, password } = req.body
+    const { user, password } = req.body
 
-    const userRepository = getCustomRepository(UserRepository)
-
-    const user = await userRepository.findOne({ where: { email } })
-
-    if (!bcrypt.compareSync(password, user.password)) {
+    if (user !== process.env.ADMIN_ROOT_USER) {
+      throw new AppError('wrong user', 403)
+    }
+    if (!bcrypt.compareSync(password, process.env.ADMIN_ROOT_PASSWORD)) {
       throw new AppError('wrong password or email', 403)
     }
 
-    const token = JWT.sign({ id: user.id }, process.env.SECRET, {
+    const token = JWT.sign({ id: user }, process.env.SECRET, {
       expiresIn: '2h',
     })
 
