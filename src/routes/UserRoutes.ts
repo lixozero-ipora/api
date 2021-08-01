ec8b49f99b12a099a53a4475d9c7150ec2870961
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 00:08:15 -0300

    refactor: remove user related

--- src/routes/UserRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import UserController from '../controllers/UserController'
-
-const route = Router()
-
-route.post('/users', UserController.store)
-
-export default route
