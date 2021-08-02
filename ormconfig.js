3051f62f29325b085d18d3120c9a472d22eb190d
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 21:11:32 -0300

    feat: use js or ts entities

--- ormconfig.js
+++ ormconfig.js
@@ -4,5 +4,5 @@ module.exports = {
   useNewUrlParser: true,
   synchronize: true,
   useUnifiedTopology: true,
-  entities: ['./{src,dist}/**/*.entity.js'],
+  entities: ['./{src,dist}/**/*.entity.{js,ts}'],
 }
