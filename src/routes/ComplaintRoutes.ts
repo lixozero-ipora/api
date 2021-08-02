1d415c90e7b9b9917148724517630ab17b051d9f
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-01 21:12:59 -0300

    feat: add index route

--- src/routes/ComplaintRoutes.ts
+++ src/routes/ComplaintRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import ComplaintController from '../controllers/ComplaintController'
+import authMiddleware from '../middleware/Auth'
 
 const route = Router()
 
 const complaintController = new ComplaintController()
 
 route.post('/', complaintController.store)
+route.get('/', authMiddleware, complaintController.index)
 
 export default route
