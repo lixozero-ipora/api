38a52bef5307e8416d276fc9733ef28c389acc46
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-08-28 13:27:26 -0300

    chore: change keep alive http package

--- src/utils/keepAlive.ts
+++ src/utils/keepAlive.ts
@@ -1,10 +1,12 @@
-import http from 'http'
+import https from 'https'
 
 setInterval(() => {
   try {
-    http.get(process.env.KEPP_ALIVE)
+    // eslint-disable-next-line no-console
+    console.log(`Keep alive to: ${process.env.KEPP_ALIVE}`)
+    https.get(process.env.KEPP_ALIVE)
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error('Tryied requisition but did not work')
+    console.error(error)
   }
 }, 900000)
